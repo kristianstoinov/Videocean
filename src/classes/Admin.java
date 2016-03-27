1d2392bc005a9d950bac4d3d19d23358c54f5608
trayanmuchev <traqn_muchev@abv.bg>
2016-03-27 18:53:41 +0300

    Admin class

---
+++ src/classes/Admin.java
@@ -0,0 +1,16 @@
+package DAO;
+
+import classes.User;
+
+public class Admin extends User {
+
+	public Admin(String username, String password, String fullName) {
+		super(username, password, fullName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Admin(int id, String email, String password, String fullName) {
+		super(id,email, password, fullName);
+	}
+
+}
