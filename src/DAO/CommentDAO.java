ad96e104883cace8288cc0e89e78fd043b4e7238
trayanmuchev <traqn_muchev@abv.bg>
2016-03-31 22:28:36 +0300

    Last DAO

--- src/DAO/CommentDAO.java
+++ src/DAO/CommentDAO.java
@@ -12,10 +12,16 @@ import exceptions.CommentException;
 public class CommentDAO extends AbstractDAO{
 	
 	
+	private static final String ADD_ANSWER_COMMENT = "INSERT INTO comments VALUES(NULL,?,?,?);";
+	private static final String ADD_MAIN_COMMENT = "INSERT INTO comments VALUES(NULL,?,?,NULL);";
+	private static final String SELECT_MAIN_COMMENT = "SELECT * FROM comments WHERE clip_id=? AND answer_comment_id IS NULL ;";
+	private static final String SELECT_ASNWER_COMMENTS = "SELECT * FROM comments WHERE clip_id=? AND answer_comment_id= ? ;";
+	private static final String DELETE_COMMENT = "DELETE FROM comments WHERE comment_id = ?;";
+
 	public List<Comment> getListMainComments(int clipID) throws CommentException{
 		
 		try{
-			PreparedStatement ps=getCon().prepareStatement("SELECT * FROM comments WHERE clip_id=? AND answer_comment_id IS NULL ;");
+			PreparedStatement ps=getCon().prepareStatement(SELECT_MAIN_COMMENT);
 			
 			ps.setInt(1, clipID);
 			ResultSet rs=ps.executeQuery();
@@ -41,7 +47,7 @@ public class CommentDAO extends AbstractDAO{
 	public List<Comment> getListAnswerComments(int clipID,int commentID) throws CommentException{
 
 		try{
-			PreparedStatement ps=getCon().prepareStatement("SELECT * FROM comments WHERE clip_id=? AND answer_comment_id= ? ;");
+			PreparedStatement ps=getCon().prepareStatement(SELECT_ASNWER_COMMENTS);
 			
 			ps.setInt(1, clipID);
 			ps.setInt(2, commentID);
@@ -59,6 +65,54 @@ public class CommentDAO extends AbstractDAO{
 		}catch(SQLException e){
 			e.printStackTrace();
 			throw new CommentException("No answer comments to show! ");
+		}
+	}
+	
+	public void removeComent(int commentId) throws CommentException {
+		PreparedStatement ps;
+		try {
+			ps = getCon().prepareStatement(DELETE_COMMENT);
+			ps.setInt(1, commentId);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			throw new CommentException("Problem with comment removing", e);
+		}
+	}
+	
+	public int addComment(Comment comment) throws CommentException{
+		PreparedStatement ps;
+		try {
+			ps = getCon().prepareStatement(ADD_MAIN_COMMENT, PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, comment.getThisClip().getClipID());
+			ps.setString(2, comment.getCommentDescription());
+
+			ps.executeUpdate();
+			ResultSet id = ps.getGeneratedKeys();
+			id.next();
+			return id.getInt(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new CommentException("Can't add main comment", e);
+		}
+	}
+	
+	public int addAnswerComment(Comment comment,int mainCommentId) throws CommentException{
+		PreparedStatement ps;
+		try {
+			ps = getCon().prepareStatement(ADD_ANSWER_COMMENT, PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, comment.getThisClip().getClipID());
+			ps.setString(2, comment.getCommentDescription());
+			ps.setInt(3,mainCommentId);
+
+			ps.executeUpdate();
+			ResultSet id = ps.getGeneratedKeys();
+			id.next();
+			return id.getInt(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new CommentException("Can't add answer comment", e);
 		}
 	}
 	
--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -3,9 +3,9 @@ package DAO;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 
 import classes.Admin;
 import classes.Clip;
@@ -20,6 +20,7 @@ import interfaces.ILanguageDAO;
 import interfaces.IUserDAO;
 
 public class UserDAO extends AbstractDAO implements IUserDAO {
+	private static final String SELECT_ALL_USERS = "SELECT * from users;";
 	private static final String GET_HISTORY = "SELECT clip_id FROM history WHERE user_id =? ;";
 	private static final String DELETE_ALL_CLIPS_OF_USER = "DELETE FROM history WHERE user_id=?;";
 	private static final String DELETE_CLIP_FROM_HISTORY = "DELETE FROM history WHERE user_id= ? and clip_id=? ;";
@@ -34,19 +35,21 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 	private static final String SELECT_FROM_USERS_WHERE_FULL_NAME_LIKE = "SELECT * from users where full_name like ?;";
 	private static final String SELECT_USER_BY_PASS_AND_EMAIL = "SELECT * from users where email like ? and password like md5(?);";
 	private static final String SELECT_FROM_USERS_BY_USER_ID = "SELECT * from users where user_id=?;";
-	private static final String DELETE_FROM_USERS_BY_ID = "DELETE FROM users WHERE id = ?;";
+	private static final String DELETE_FROM_USERS_BY_ID = "DELETE FROM users WHERE user_id = ?;";
 	private static final String ADD_USER_QUERY = "INSERT INTO users (email,md5(password),full_name) VALUES (?,?,?);";
 	private static final String UPDATE_USER_QUERY = "UPDATE users SET email = ?, password = md5(?), full_name = ? , picture=?,country_id=?,language_id=?,backgroung_picture=? WHERE id = ?;";
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see DAO.IUser#addUser(classes.User)
 	 */
 	@Override
 	public int addUser(User user) throws UserProblemException {
 		if (user != null) {
+			PreparedStatement ps;
 			try {
-				PreparedStatement ps = getCon().prepareStatement(ADD_USER_QUERY,
-						PreparedStatement.RETURN_GENERATED_KEYS);
+				ps = getCon().prepareStatement(ADD_USER_QUERY, PreparedStatement.RETURN_GENERATED_KEYS);
 				ps.setString(1, user.getUsername());
 				ps.setString(2, user.getPassword());
 				ps.setString(3, user.getFullName());
@@ -65,7 +68,9 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see DAO.IUser#updateUser(classes.User)
 	 */
 	@Override
@@ -95,7 +100,9 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see DAO.IUser#removeUser(int)
 	 */
 	@Override
@@ -110,7 +117,9 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see DAO.IUser#getUserById(int)
 	 */
 	@Override
@@ -134,7 +143,9 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see DAO.IUser#getUserByEmailAndPass(java.lang.String, java.lang.String)
 	 */
 	@Override
@@ -161,22 +172,24 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 			throw new UserProblemException("Wrong email or password");
 		}
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see DAO.IUser#getUsersByName(java.lang.String)
 	 */
 	@Override
-	public List<User> getUsersByName(String name) throws UserProblemException{
+	public List<User> getUsersByName(String name) throws UserProblemException {
 		PreparedStatement ps;
-		List<User> wantedUsers=new ArrayList<User>();
-		if (name!=null) {
+		List<User> wantedUsers = new ArrayList<User>();
+		if (name != null) {
 			try {
 				ps = getCon().prepareStatement(SELECT_FROM_USERS_WHERE_FULL_NAME_LIKE);
-				ps.setString(1, "%"+name+"%");
+				ps.setString(1, "%" + name + "%");
 
 				ResultSet rs = ps.executeQuery();
-				while(rs.next()){
-				wantedUsers.add(getWantedUser(rs));
+				while (rs.next()) {
+					wantedUsers.add(getWantedUser(rs));
 				}
 				return wantedUsers;
 			} catch (SQLException | PictureFormatException | UserProblemException e) {
@@ -188,6 +201,25 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 			throw new UserProblemException("Wrong Id");
 		}
 	}
+	
+	public List<User> getAllUsers() throws UserProblemException {
+		Statement st;
+		List<User> wantedUsers = new ArrayList<User>();
+			try {
+				st = getCon().createStatement();
+
+				ResultSet rs = st.executeQuery(SELECT_ALL_USERS);
+				while (rs.next()) {
+					wantedUsers.add(getWantedUser(rs));
+				}
+				return wantedUsers;
+			} catch (SQLException | PictureFormatException | UserProblemException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new UserProblemException("Can't return users.Please try again", e);
+			}
+		}
+	
 
 	private User getWantedUser(ResultSet rs) throws SQLException, PictureFormatException, UserProblemException {
 		User wantedUser;
@@ -213,175 +245,173 @@ public class UserDAO extends AbstractDAO implements IUserDAO {
 		wantedUser.setBackgroundPicture(backgroundPicture);
 		return wantedUser;
 	}
-	
-	public void addPlaylistIntoLibrary(int playlistId,int userId) throws UserProblemException{
+
+	public void addPlaylistIntoLibrary(int playlistId, int userId) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(ADD_INTO_LIBRARY);
+			ps = getCon().prepareStatement(ADD_INTO_LIBRARY);
 			ps.setInt(1, userId);
-			ps.setInt(2,playlistId);
-			
+			ps.setInt(2, playlistId);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new UserProblemException("Can't add playlist.Please try again", e);
 		}
-		}
-	
-	public void deletePlaylistFromLibrary(int playlistId,int userId) throws UserProblemException{
+	}
+
+	public void deletePlaylistFromLibrary(int playlistId, int userId) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(DELETE_FROM_LIBRARY);
-			ps.setInt(1,userId);
-			ps.setInt(2,playlistId);
-			
+			ps = getCon().prepareStatement(DELETE_FROM_LIBRARY);
+			ps.setInt(1, userId);
+			ps.setInt(2, playlistId);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new UserProblemException("Can't remove users playlist",e);
+			throw new UserProblemException("Can't remove users playlist", e);
 		}
 	}
-	
-	public List<Playlist> getAllPlaylistsForUser(int userId) throws UserProblemException{
-		List<Playlist> playlists=new ArrayList<Playlist>();
+
+	public List<Playlist> getAllPlaylistsForUser(int userId) throws UserProblemException {
+		List<Playlist> playlists = new ArrayList<Playlist>();
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(SELECT_PLAYLISTS_FROM_LIBRARY);
-			ps.setInt(1,userId);
-			
-			ResultSet rs=ps.executeQuery();
-			PlaylistDAO playlist=new PlaylistDAO();
-			while(rs.next()){
+			ps = getCon().prepareStatement(SELECT_PLAYLISTS_FROM_LIBRARY);
+			ps.setInt(1, userId);
+
+			ResultSet rs = ps.executeQuery();
+			PlaylistDAO playlist = new PlaylistDAO();
+			while (rs.next()) {
 				playlists.add(playlist.getPlaylistById(rs.getInt(1)));
 			}
 			return playlists;
 		} catch (SQLException | PlaylistException e) {
 			e.printStackTrace();
-			throw new UserProblemException("There is a problem.We can't get you back your playlists for now",e);
+			throw new UserProblemException("There is a problem.We can't get you back your playlists for now", e);
 		}
 	}
-	
-	public void addLike(int userId,int clipId,int preference) throws UserProblemException{
+
+	public void addLike(int userId, int clipId, int preference) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(ADD_INTO_LIKES);
-			ps.setInt(1,userId);
-			ps.setInt(2,clipId);
-			ps.setInt(3,preference);
-			
+			ps = getCon().prepareStatement(ADD_INTO_LIKES);
+			ps.setInt(1, userId);
+			ps.setInt(2, clipId);
+			ps.setInt(3, preference);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new UserProblemException("Like doesn't insert", e);
 		}
 	}
-	
-	public void updateLike(int userId,int clipId,int preference) throws UserProblemException{
+
+	public void updateLike(int userId, int clipId, int preference) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(UPDATE_LIKES);
-			ps.setInt(1,preference);
-			ps.setInt(2,userId);
-			ps.setInt(3,clipId);
-			
+			ps = getCon().prepareStatement(UPDATE_LIKES);
+			ps.setInt(1, preference);
+			ps.setInt(2, userId);
+			ps.setInt(3, clipId);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new UserProblemException("Like doesn't update", e);
 		}
 	}
-	
-	public void deleteLike(int userId,int clipId) throws UserProblemException{
+
+	public void deleteLike(int userId, int clipId) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(DELETE_FROM_LIKES);
-			ps.setInt(1,userId);
-			ps.setInt(2,clipId);
-			
+			ps = getCon().prepareStatement(DELETE_FROM_LIKES);
+			ps.setInt(1, userId);
+			ps.setInt(2, clipId);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new UserProblemException("Like doesn't delete", e);
 		}
 	}
-	
-	public int getCountFromLikes(int clipId,int preference) throws UserProblemException{
+
+	public int getCountFromLikes(int clipId, int preference) throws UserProblemException {
 		PreparedStatement ps;
-		int count=0;
+		int count = 0;
 		try {
-			ps=getCon().prepareStatement(SELECT_COUNT_LIKES_OR_DISLIKES);
-			ps.setInt(1,clipId);
-			ps.setInt(2,preference);
-			
-			ResultSet rs=ps.executeQuery();
+			ps = getCon().prepareStatement(SELECT_COUNT_LIKES_OR_DISLIKES);
+			ps.setInt(1, clipId);
+			ps.setInt(2, preference);
+
+			ResultSet rs = ps.executeQuery();
 			rs.next();
-			count=rs.getInt(1);
+			count = rs.getInt(1);
 			return count;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new UserProblemException("Can't count likes/dislikes",e);
+			throw new UserProblemException("Can't count likes/dislikes", e);
 		}
 	}
-	
-	public void addClipToHistory(int clipID,int userID) throws UserProblemException
-	{
+
+	public void addClipToHistory(int clipID, int userID) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(ADD_TO_HISTORY);
+			ps = getCon().prepareStatement(ADD_TO_HISTORY);
 			ps.setInt(1, userID);
-			ps.setInt(2,clipID);
-			
+			ps.setInt(2, clipID);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new UserProblemException("Can't insert into history", e);
 		}
 	}
-	
-	public void deleteClipFromHistory(int clipID,int userID) throws UserProblemException{
+
+	public void deleteClipFromHistory(int clipID, int userID) throws UserProblemException {
 		PreparedStatement ps;
 		try {
-			ps=getCon().prepareStatement(DELETE_CLIP_FROM_HISTORY);
-			ps.setInt(1,userID);
-			ps.setInt(2,clipID);
-			
+			ps = getCon().prepareStatement(DELETE_CLIP_FROM_HISTORY);
+			ps.setInt(1, userID);
+			ps.setInt(2, clipID);
+
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new UserProblemException("Can't remove clip",e);
+			throw new UserProblemException("Can't remove clip", e);
 		}
 	}
-	
-	public void deleteAllClipsFromHistory(int userID) throws UserProblemException
-	{
+
+	public void deleteAllClipsFromHistory(int userID) throws UserProblemException {
 		PreparedStatement ps;
-	try {
-		ps=getCon().prepareStatement(DELETE_ALL_CLIPS_OF_USER);
-		ps.setInt(1,userID);
-		ps.executeUpdate();
-	} catch (SQLException e) {
-		e.printStackTrace();
-		throw new UserProblemException("Can't remove clips",e);
-	}
-	}
-	
-	public List<Clip> getHistory(int userID) throws UserProblemException{
-	List<Clip> history=new ArrayList<Clip>();
-	PreparedStatement ps;
-	try {
-		ps=getCon().prepareStatement(GET_HISTORY);
-		ps.setInt(1,userID);
-		
-		ResultSet rs=ps.executeQuery();
-		ClipDAO clipDAO=new ClipDAO();
-		while(rs.next()){
-			history.add(clipDAO.getClipByID(rs.getInt(1)));
+		try {
+			ps = getCon().prepareStatement(DELETE_ALL_CLIPS_OF_USER);
+			ps.setInt(1, userID);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new UserProblemException("Can't remove clips", e);
 		}
-		return history;
-	} catch (SQLException | ClipException e) {
-		e.printStackTrace();
-		throw new UserProblemException("There is a problem.We can't get your history",e);
 	}
+
+	public List<Clip> getHistory(int userID) throws UserProblemException {
+		List<Clip> history = new ArrayList<Clip>();
+		PreparedStatement ps;
+		try {
+			ps = getCon().prepareStatement(GET_HISTORY);
+			ps.setInt(1, userID);
+
+			ResultSet rs = ps.executeQuery();
+			ClipDAO clipDAO = new ClipDAO();
+			while (rs.next()) {
+				history.add(clipDAO.getClipByID(rs.getInt(1)));
+			}
+			return history;
+		} catch (SQLException | ClipException e) {
+			e.printStackTrace();
+			throw new UserProblemException("There is a problem.We can't get your history", e);
+		}
 	}
-	
+
 }
