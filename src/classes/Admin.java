63ab53bf765fb9bb939ebffc3ddafaa77a91fe9a
trayanmuchev <traqn_muchev@abv.bg>
2016-03-31 22:25:16 +0300

    Admin

--- src/classes/Admin.java
+++ src/classes/Admin.java
@@ -1,16 +1,41 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import classes.User;
 
 public class Admin extends User {
-
+private Map<Integer,User> users;
 	public Admin(String username, String password, String fullName) {
 		super(username, password, fullName);
-		// TODO Auto-generated constructor stub
+		users=new HashMap<Integer,User>();
 	}
 
 	public Admin(int id, String email, String fullName) {
 		super(id,email, fullName);
+		users=new HashMap<Integer,User>();
 	}
+	
+	public void addUser(User user){
+		users.put(user.getUserID(), user);
+	}
+	
+	public void removeUser(int userId){
+		users.remove(userId);
+	}
+	
+	public List<User> getUsers(){
+		List<User> wantedUsers=new ArrayList<User>();
+		User forAdding;
+		for(int key:users.keySet()){
+			forAdding=users.get(key);
+			wantedUsers.add(forAdding);
+		}
+		return wantedUsers;
+	}
+
 
 }
