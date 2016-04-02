0cc041cf7961a7cce2814f1691f4ecc88cfedaeb
Radoslav Yanchev <radoslav.yanchev86@gmail.com>
2016-04-02 12:53:36 +0300

    Refactor TestPlaylistDAO

--- src/tests/TestPlaylistDAO.java
+++ src/tests/TestPlaylistDAO.java
@@ -1,6 +1,8 @@
-package Tests;
+package tests;
 
 import static org.junit.Assert.*;
+
+import java.sql.SQLException;
 
 import org.junit.Test;
 
@@ -22,27 +24,27 @@ public class TestPlaylistDAO {
 	@Test
 	public void creatPlaylist() throws UserProblemException, PlaylistException {
 		UserDAO userDAO = new UserDAO();
-		User user = userDAO.getUserById(2);
+		User user = userDAO.getUserById(1);
 		IPlaylistDAO playlistDAO = new PlaylistDAO();
 		Playlist playlist = new Playlist("MoqPlaylist", user, TYPE.PUBLIC);
 		int id = playlistDAO.createPlaylist(playlist);
-		playlistDAO.removePlaylistByID(id);
+//		playlistDAO.removePlaylistByID(id);
 	}
 
 	@Test
-	public void addOrRemoveClipToPlayList() throws ClipException, PlaylistException {
+	public void addOrRemoveClipToPlayList() throws ClipException, PlaylistException, SQLException {
 		ClipDAO clipDAO = new ClipDAO();
-		Clip clip = clipDAO.getClipByID(2);
-		PlaylistDAO playlistDAO = new PlaylistDAO();
-		playlistDAO.addClipToPlaylist(10, clip.getClipID());
-		playlistDAO.removeClipFromPlaylist(10, 2);
+		Clip clip = clipDAO.getClipByID(1);
+		interfaces.IPlaylistDAO playlistDAO = new PlaylistDAO();
+		playlistDAO.addClipToPlaylist(1, clip.getClipID());
+		playlistDAO.removeClipFromPlaylist(1, 1);
 	}
-	@Test
-	public void increaseViewsOfPlaylist() throws PlaylistException{
-		IPlaylistDAO playlistDAO = new PlaylistDAO();
-		Playlist playlist = playlistDAO.getPlaylistById(10);
-		playlistDAO.increaseViewsOfPlaylist(playlist);
-	}
-	
+
+	// @Test
+	// public void increaseViewsOfPlaylist() throws PlaylistException {
+	// IPlaylistDAO playlistDAO = new PlaylistDAO();
+	// Playlist playlist = playlistDAO.getPlaylistById(10);
+	// playlistDAO.increaseViewsOfPlaylist(playlist);
+	// }
 
 }
