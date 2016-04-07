237dd72d1dbede6c09436a99197fafc53f5b822c
Christian Stoynov <kristian_stoinov@abv.bg>
2016-04-07 15:20:30 +0300

    even more scraping

--- MyProject/src/main/java/com/example/model/ClientsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.model;
-
-public class ClientsDAO {
-
-	
-	public static void addNewClient(Client client){
-		System.out.println("New client added with name = " + client.getName() + " and salary " + client.getSalary());
-	}
-}
